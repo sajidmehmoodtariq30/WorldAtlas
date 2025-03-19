972b40be73b4c53220b7c98e735935f91a4bc107
Sajid M Tariq <sajidmehmoodtariq5@gmail.com>
2025-03-19 11:23:04 +0500

    setting tailwind css

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -3,10 +3,17 @@ import { NavLink } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>
-      <h1>Oops! An error occurred</h1>
-      <p>The page you are looking for does not exist.</p>
-      <NavLink to='/'><button>GO Back</button></NavLink>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+      <div className="text-center animate-pulse mb-8">
+        <h1 className="text-6xl font-bold mb-4">404</h1>
+        <p className="text-xl">Oops! The page you're looking for doesn't exist.</p>
+      </div>
+      <NavLink
+        to="/"
+        className="px-6 py-3 text-lg font-semibold text-white bg-blue-600 rounded-full hover:bg-blue-700 transition-all duration-300"
+      >
+        Go Back Home
+      </NavLink>
     </div>
   )
 }
